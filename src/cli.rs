fe0ddb88eec814a5833b063a3ad84b23c681d113
poscat <poscat@poscat.moe>
2022-12-30 17:11:12 +0800

    add more options to CLI parser
    - short form and long form of cli flags
    - default values

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,19 @@
-use clap::Parser;
+pub use clap::Parser;
+
+const V4_DEFAULT_URL: &str = "";
+const V6_DEFAULT_URL: &str = "";
 
 #[derive(Parser)]
+#[command(author, version, about)]
 pub struct Arg {
+    #[arg(short = '4', long = "v4-url", default_value_t = String::from(V4_DEFAULT_URL))]
     pub v4_url: String,
+    #[arg(short = '6', long = "v6-url", default_value_t = String::from(V6_DEFAULT_URL))]
     pub v6_url: String,
+    #[arg(long = "no-v6", default_value_t = false)]
     pub no_v6: bool,
+    #[arg(short = 'i', long = "interface")]
     pub interface: String,
+    #[arg(short = 't', long = "table")]
     pub table: u64
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ mod cli;
 
 use rtnetlink::{new_connection, Handle};
 use anyhow::{Result, Context};
+use cli::*;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let arg = Arg::parse();
     let (conn, handle, _) = new_connection().context("")?;
     tokio::spawn(conn);
 
